4fdb96cdc01442971d5f7f902f03f5b9101c8962
Erling Stage <erling.stage@gmail.com>
2016-01-31 11:28:18 +0200

    checkpoint

--- stsched.py
+++ stsched.py
@@ -6,8 +6,7 @@ import re
 """
 Interpret systemtap logfile with scheduling information
 
-@481:481>(RCC_HMI)    start process - parentheses indicate NAME of executable
-
+@481:481>(RCC_HMI)    start process/thread  (pid:tid)
 #0>529                schedule to thread 529
 
 0000000000111111111
@@ -27,7 +26,7 @@ class thread :
         self.tid = tid
         self.start = time
         self.names = [name]
-        self.buckets = [0 for i in range(buckets)]
+        self.buckets = [0 for i in range(buckets+1)]
         self.totalTime = 0  # Total CPU time in this thread
         return
         
@@ -43,6 +42,10 @@ class thread :
             self.buckets[self.bucket] += part1
             self.sliceStart += part1
             slice += part1
+        #if bucket-self.bucket > 1:
+            #print(bucket, self.bucket, time, btime)
+            #assert(bucket-self.bucket <= 1) # assume scheduling happens more than once per bucketTime
+            
         self.buckets[bucket] += time - self.sliceStart
         slice += time - self.sliceStart
         return slice
@@ -53,18 +56,23 @@ class thread :
         return
 
 class kernel :
-    def __init__(self, bucketCount):
+    def __init__(self, bucketCount, bucketTime, processCount):
         self.processes = {}   # Map pid -> [tid]+
         self.threadName = {}  # Map tid -> name
         self.threads = {}     # Map tid -> thread
         self.tidpid = {}      # map tid -> pid
         self.pidtime = {}     # map pid -> cpu time
         self.bucketCount = bucketCount
+        self.bucketTime = bucketTime
         self.lastThread = None
+        self.firstTime = None # Timestamp of 1st trace
+        self.processCount = processCount # How many processes to graph (with rest summarized under misc)
         return
 
     def handleScheduling(self, pid, tid, time, btime, bucket):
         if self.lastThread:
+            if self.firstTime == None:
+                self.firstTime = time
             self.lastThread.scheduleOut(time, btime, bucket)
         return
 
@@ -106,7 +114,7 @@ class kernel :
     def printProcesses(self, lastBucket, f = sys.stdout):
         k = self.processes.keys()
         for x in k:
-            print('{:4d} {:12.9f} "{:s}"'.format(x, self.pidtime[x]/1000000000.0, self.threadName[x]), file=f)
+            print('{:4d} {:12.9f} "{:s}"'.format(x, self.pidtime[x]/self.bucketTime, self.threadName[x]), file=f)
         return
     
     def printTable(self, lastBucket, f = sys.stdout):
@@ -123,11 +131,73 @@ class kernel :
             for t in threadList:
                 thr = self.threads[t]
                 for bucket in range(lastBucket+1):
-                    timing[bucket] += thr.buckets[bucket]/100000000.0
+                    timing[bucket] += thr.buckets[bucket]/self.bucketTime
             # Output timing for process
             print('"{0}"'.format(p[2]),timing)
         return
     
+    def printTable2(self, lastBucket, f=sys.stdout):
+        maxProcessCount = self.processCount
+        # Create a list of processes ordred in descending orderby total cpu time
+        k = self.processes.keys()
+        pl = []
+        for x in k:
+            pl.append([x,self.pidtime[x],self.threadName[x]])
+        p2 = sorted(pl, key=lambda rec: rec[1], reverse=True)
+
+        # First row is "Time","Process0","Process1"..."Process99","Misc"
+        processCount = len(p2)
+        titleRow = ['"uptime (seconds)"']
+        for p in p2[:min(maxProcessCount, processCount)]:
+            titleRow.append('"{0}"'.format(self.threadName[p[0]]))
+        if processCount > maxProcessCount:
+            titleRow.append("Misc")
+
+        # Create an array to hold a column of data for time, each process [and misc]
+        dataRow = [None for i in range(min(maxProcessCount, processCount)+2)]
+
+        # Create column of start times
+        timeColumn = [0 for i in range(lastBucket+1)]
+        timeColumn[0] = self.firstTime - (self.firstTime % self.bucketTime)
+        for bucket in range(lastBucket):
+            timeColumn[bucket+1] = timeColumn[bucket] + self.bucketTime
+        dataRow[0] = timeColumn
+        
+        # Create a column of data for each of the maxProcessCount most active processes
+        idx = 1
+        for p in p2[:min(processCount,maxProcessCount)]:
+            timing = [0 for i in range(lastBucket+1)]
+            threadList = self.processes[p[0]]
+            for t in threadList:
+                thr = self.threads[t]
+                for bucket in range(lastBucket+1):
+                    timing[bucket] += thr.buckets[bucket]/self.bucketTime
+            dataRow[idx] = timing
+            idx += 1
+
+        # Summarize data for processes beyond the maxProcessCount most active
+        if processCount > maxProcessCount:
+            # An empty array for the sum
+            summarize = [0 for i in range(lastBucket+1)]
+            for p in p2[maxProcessCount:]:
+                threadList = self.processes[p[0]]
+                for t in threadList:
+                    thr = self.threads[t]
+                    for bucket in range(lastBucket+1):
+                        summarize[bucket] += thr.buckets[bucket]/self.bucketTime
+            dataRow[idx] = summarize
+            
+        # Time to output data
+        for t in titleRow:
+            print("{0},".format(t),end='',file=f)
+        print(file=f)
+        for i in range(lastBucket):
+            print("{:11.9f},".format(dataRow[0][i]/1.0E9),end='',file=f)
+            for x in range(1,idx+1):
+                print("{:11.9f},".format(dataRow[x][i]),end='',file=f)
+            print(file=f)
+        return
+        
 #                               pid   tid   name
 processPattern = re.compile(r'@(\d+):(\d+)>(.*)')
 schedPattern   = re.compile(r'#(\d)>(\d+)')
@@ -135,34 +205,43 @@ schedPattern   = re.compile(r'#(\d)>(\d+)')
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("trek", help="systemtap/taptrek logfile")
+    parser.add_argument("--bucket", type=int, help="Length of sampling bucket in ns", default=100000000)
+    parser.add_argument("--duration", type=float, help="Skip data beyond 'duration' seconds")
+    parser.add_argument("--pcount", type=int, help="Number of ranked process to display", default=100)
+
     args = parser.parse_args()
     
-    bucketCount = 2000
-    bucketTime = 100000000 # 10^8 = 0.1s
-    kern = kernel(bucketCount)
+    bucketTime = float(args.bucket) #100000000.0 # 10^8 = 0.1s
+    if args.duration:
+        bucketCount = int((args.duration * 1E9 / bucketTime)+1)
+        durationNs = args.duration * 1E9 
+    else:
+        durationNs = 0
+        bucketCount = 20000 # some arbitrary  number
+    kern = kernel(bucketCount, bucketTime, args.pcount)
     
     with open(args.trek) as f:
         for line in f:
-#            print(line)
             bracket = line.find(']')
             time = int(line[1:bracket-2])
-            #ns = line[bracket-2:bracket]
-            tenths = int(line[1:bracket-10])
+            if durationNs and (time > durationNs):
+                break
+            #tenths = int(line[1:bracket-10])
+            bucket = int(time / args.bucket)
             btime = time - (time % bucketTime)
-            #print(time, btime, tenths)
             res = processPattern.search(line)
             if res != None:
                 pid = int(res.group(1))
                 tid = int(res.group(2))
                 name = res.group(3)
-                kern.processStart(pid, tid, time, btime, tenths, name)
+                kern.processStart(pid, tid, time, btime, bucket, name)
             else:
                 res = schedPattern.search(line)
                 if res != None:
                     tid = int(res.group(2))
-                    kern.schedule(tid, time, btime, tenths)
+                    kern.schedule(tid, time, btime, bucket)
     #kern.printProcesses()
-    kern.printTable(tenths)
+    kern.printTable2(bucket)
     return
 
 if __name__ == '__main__':
